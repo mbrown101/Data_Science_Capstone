e05a5647971512d6a1dddd4661b4a38d2f790d26
mbrown101 <m.g.brown85@gmail.com>
2016-01-03 19:58:21 -0800

    Update server.R

--- server.R
+++ server.R
@@ -1,21 +1,28 @@
 # server.R
-
+options(shiny.maxRequestSize=30*1024^2)
 library(shiny)
 library(plyr)          # data ordering
 library(reshape)
-oracle <- readRDS("data/three_gram_oracle.rds")
+oracle3gram <- readRDS("data/three_gram_oracle.rds")
 
 shinyServer(
 
   function(input, output) {
     
-    output$text1 <- renderText({ 
-      arrange(oracle[ which(oracle$leading_Bigram == input$stringInput & oracle$count > 1) , ] , -count)[1,4]         
+    output$textWords <- renderText({
+      paste(unlist(strsplit(input$stringInput , " ")) , "")
     })
     
-    # sorted columns are colored now because CSS are attached to them
+    output$text1 <- renderText({ 
+      if (length(unlist(strsplit(input$stringInput , " "))) == 2){
+      arrange(oracle3gram[ which(oracle3gram$leading_Bigram == input$stringInput & oracle3gram$count > 1) , ] , -count)[1,4]         
+    } else {
+      "dog"
+    }
+   })
+    
     output$matchTable = renderDataTable({
-      arrange(oracle[ which(oracle$leading_Bigram == input$stringInput ) , ], -count)[1:input$sliderGuesses,]
+      arrange(oracle3gram[ which(oracle3gram$leading_Bigram == input$stringInput ) , ], -count)[1:input$sliderGuesses,]
     }, options = list(orderClasses = TRUE , searching = FALSE , paging = FALSE))
 
   }

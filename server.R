f4746ca8b6fe9e7cf02fc64eb27ef7b8b5d446ea
mbrown101 <m.g.brown85@gmail.com>
2016-01-10 20:09:10 -0800

    Update server.R

--- server.R
+++ server.R
@@ -3,27 +3,82 @@ options(shiny.maxRequestSize=30*1024^2)
 library(shiny)
 library(plyr)          # data ordering
 library(reshape)
-oracle3gram <- readRDS("data/three_gram_oracle.rds")
+library(stringr)
 
-shinyServer(
+oracle3gram <- readRDS("data/project.data.3df.rds")
+oracle2gram <- readRDS("data/project.data.2df.rds")
 
-  function(input, output) {
-    
-    output$textWords <- renderText({
-      paste(unlist(strsplit(input$stringInput , " ")) , "")
-    })
+#rows2 <- paste("rows in 2 gram =" , nrow(oracle2gram))
+rows3 <- paste("rows in 3 gram =" , nrow(oracle3gram))
+
+shinyServer(function(input, output) {
+  
+# newdata <- mtcars[order(mpg, cyl),]
+  
+  #### rows in oracles    
+  #output$text2Gram <- renderText({return(rows2)})
+  output$text3Gram <- renderText({return(rows3)})
+  
+  output$textWords <- renderText({
+    tolower(input$stringInput)
+    #paste(unlist(strsplit(input$stringInput , " ")) , "")
+  })  ## This is the input redirecte to output to the UI
+
+  #ifelse(test, yes, no)
+  #stri_stats_latex(input$stringInput)[4]
+  
+  ### Form strings to search against
+  #termTail1 <- function() return( trimws( tail( unlist( strsplit( input$stringInput , ' ') ) , 1) ) )
+  #termTail2 <- function() return( trimws(paste(tail(unlist(strsplit(input$stringInput , ' ')) , 2) , ' ')) )
+  #termTail3 <- function() return( trimws(paste(tail(unlist(strsplit(input$stringInput , ' ')) , 3) , ' ')) )
+  #termTail4 <- function() return( trimws(paste(tail(unlist(strsplit(input$stringInput , ' ')) , 4) , ' ')) )
+  
+  termTail1 <- function() return( ifelse( !is.null(input$stringInput)  , word( input$stringInput , -1) , NULL ) )
+  termTail2 <- function() return( ifelse( !is.null(input$stringInput)*!is.null(termTail1())*!is.na(word( input$stringInput , -2)) , paste( word( input$stringInput , -2) , termTail1() ) , NULL ) )
+  termTail3 <- function() return( ifelse( !is.null(input$stringInput)*!is.null(termTail2())*!is.na(word( input$stringInput , -3)) , paste( word( input$stringInput , -3) , termTail2() ) , NULL ) )
+  termTail4 <- function() return( ifelse( !is.null(input$stringInput)*!is.na(word( input$stringInput , -4)) , paste( word( input$stringInput , -4) , termTail3() ) , NULL ) )
+  
+  output$formed1G <- renderText({ termTail1() })
+  output$formed2G <- renderText({ termTail2() })
+  output$formed3G <- renderText({ termTail3() })
+  output$formed4G <- renderText({ termTail4() })
+  
+
+  ### Word guess
     
     output$text1 <- renderText({ 
       if (length(unlist(strsplit(input$stringInput , " "))) == 2){
-      arrange(oracle3gram[ which(oracle3gram$leading_Bigram == input$stringInput & oracle3gram$count > 1) , ] , -count)[1,4]         
-    } else {
-      "dog"
-    }
+          arrange(oracle3gram[ which(oracle3gram$leadingGram == tolower(trimws(input$stringInput))) , ] , -count)[1,4]         
+      } else {"Nothing to see here"}
    })
     
-    output$matchTable = renderDataTable({
-      arrange(oracle3gram[ which(oracle3gram$leading_Bigram == input$stringInput ) , ], -count)[1:input$sliderGuesses,]
-    }, options = list(orderClasses = TRUE , searching = FALSE , paging = FALSE))
+    
+    output$text2 <- renderText({ 
+      if (length(unlist(strsplit(termTail2() , " "))) == 2){
+        arrange(oracle3gram[ which( oracle3gram$leadingGram == tolower( trimws( termTail2() )) ) , ] , -count)[1,4]         
+      } else {"Nothing to see here"}
+    })
+    
+    output$text3 <- renderText({ 
+      if (stri_stats_latex(input$stringInput)[4] > 1 & !is.null(input$stringInput) ){
+        arrange(oracle3gram[ which( oracle3gram$leadingGram == tolower( trimws( termTail2() )) ) , ] , -count)[1,4]         
+      } 
+    })
+    
+    
+    
+#    output$matchTable = renderDataTable({
+#      arrange(oracle3gram[ which(oracle3gram$leadingGram == input$stringInput ) , ], -count)[1:input$sliderGuesses,]
+#    }, options = list(orderClasses = TRUE , searching = FALSE , paging = FALSE))
 
-  }
-)
+    output$matchTable = renderDataTable({ 
+      if (length(unlist(strsplit(input$stringInput , " "))) == 2){
+        arrange(oracle3gram[ which(oracle3gram$leadingGram == tolower(trimws(input$stringInput)) ) , ], -count)[1:input$sliderGuesses,]
+        }
+      
+      if (length(unlist(strsplit(input$stringInput , " "))) == 1){
+        arrange(oracle2gram[ which(oracle2gram$leadingGram == tolower(trimws(input$stringInput)) ) , ], -count)[1:input$sliderGuesses,]
+      }}, options = list(orderClasses = TRUE , searching = FALSE , paging = FALSE))    
+    
+    
+})  # close function input / output

352dc803f4f74c343aa11435917a1de627b7d87b
mbrown101 <m.g.brown85@gmail.com>
2016-01-14 00:12:54 -0800

    Update server.R

--- server.R
+++ server.R
@@ -22,73 +22,72 @@ shinyServer(function(input, output) {
   last_3 <- reactive({ tail(strsplit(trimws(input$stringInput),split=" ")[[1]],3) })
   last_4 <- reactive({ tail(strsplit(trimws(input$stringInput),split=" ")[[1]],4) })
   
-  
-# output$w_1 <- renderText({ last_1() })
-#  output$w_2 <- renderText({ last_2() })
-#  output$w_3 <- renderText({ last_3() })
-#  output$w_4 <- renderText({ last_4() })
+  output$w_1 <- renderText({ last_1() })
+  output$w_2 <- renderText({ last_2() })
+  output$w_3 <- renderText({ last_3() })
+  output$w_4 <- renderText({ last_4() })
   
   
   ### Word guesses
 
-#    output$gram2 <- renderText({ 
-#      if ( length(last_1()) == 1  ){ 
-#         arrange(oracle2gram[ which(oracle2gram$leadingGram == tolower( trimws( paste(last_1() , collapse = ' ')))) , ], -count)[1,3] 
-#      } else {"input length != 1"}
-#    }) 
+    output$gram2 <- renderText({ 
+      if ( length(last_1()) == 1  ){ 
+         arrange(oracle2gram[ which(oracle2gram$leading_Gram == tolower( trimws( paste(last_1() , collapse = ' ')))) , ], -frequency)[1,2] 
+      } else {"input length != 1"}
+    }) 
         
   
-#    output$gram3 <- renderText({ 
-#      if ( length(last_2()) == 2 ) {
-#         arrange(oracle3gram[ which( oracle3gram$leadingGram == tolower( trimws( paste(last_2() , collapse = ' ')))) , ] , -count)[1,3]         
-#      } else {"Absoultly Nothing to see there"}
-#    })
+    output$gram3 <- renderText({ 
+      if ( length(last_2()) == 2 ) {
+         arrange(oracle3gram[ which( oracle3gram$leading_Gram == tolower( trimws( paste(last_2() , collapse = ' ')))) , ] , -frequency)[1,2]         
+      } else {"Absoultly Nothing to see there"}
+    })
     
     
-#    output$gram4 <- renderText({ 
-#      if ( length(last_3()) == 3 ){
-#         arrange(oracle4gram[ which( oracle4gram$leadingGram == tolower( trimws( paste(last_3() , collapse = ' ')))) , ] , -count)[1,4]         
-#      } else {"Totally Nothing to see there"}
-#    })
+    output$gram4 <- renderText({ 
+      if ( length(last_3()) == 3 ){
+         arrange(oracle4gram[ which( oracle4gram$leading_Gram == tolower( trimws( paste(last_3() , collapse = ' ')))) , ] , -frequency)[1,2]         
+      } else {"Totally Nothing to see there"}
+    })
     
-#    output$gram5 <- renderText({ 
-#      if ( length(last_4()) == 4  ){
-#          arrange(oracle5gram[ which( oracle5gram$leadingGram == tolower( trimws( paste(last_4() , collapse = ' ')))) , ] , -count)[1,4]         
-#      } else {"Ludicracy Nothing to see there"}
-#    })
+    output$gram5 <- renderText({ 
+      if ( length(last_4()) == 4  ){
+          arrange(oracle5gram[ which( oracle5gram$leading_Gram == tolower( trimws( paste(last_4() , collapse = ' ')))) , ] , -frequency)[1,2]         
+      } else {"Ludicracy Nothing to see there"}
+    })
     
     
     
     
     logical2 <- reactive({ 
       if ( length(last_1()) == 1 ){
-        arrange(oracle2gram[ which( oracle2gram$leadingGram == tolower( trimws( paste(last_1() , collapse = ' ')))) , ] , -count)[1,3]         
-      } 
+        arrange(oracle2gram[ which( oracle2gram$leading_Gram == tolower( trimws( paste(last_1() , collapse = ' ')))) , ] , -frequency)[1,2]         
+      } else {NA}
     })
     
     logical3 <- reactive({ 
       if ( length(last_2()) == 2 ){
-        arrange(oracle3gram[ which( oracle3gram$leadingGram == tolower( trimws( paste(last_2() , collapse = ' ')))) , ] , -count)[1,3]         
-      } 
+        arrange(oracle3gram[ which( oracle3gram$leadingGram == tolower( trimws( paste(last_2() , collapse = ' ')))) , ] , -frequency)[1,2]         
+      } else {NA}
     })
     
     logical4 <- reactive({ 
       if ( length(last_3()) == 3 ){
-        arrange(oracle4gram[ which( oracle4gram$leadingGram == tolower( trimws( paste(last_3() , collapse = ' ')))) , ] , -count)[1,4]         
-      }  
+        arrange(oracle4gram[ which( oracle4gram$leading_Gram == tolower( trimws( paste(last_3() , collapse = ' ')))) , ] , -frequency)[1,2]         
+      }  else {NA}
     })
     
     logical5 <- reactive({ 
       if ( length(last_4()) == 4  ){
-        arrange(oracle5gram[ which( oracle5gram$leadingGram == tolower( trimws( paste(last_4() , collapse = ' ')))) , ] , -count)[1,4]         
-      } 
+        arrange(oracle5gram[ which( oracle5gram$leading_Gram == tolower( trimws( paste(last_4() , collapse = ' ')))) , ] , -frequency)[1,2]         
+      } else {NA}
     })
     
     output$mesh <- reactive({
-       ifelse( !is.null(logical5()) , logical5() , 
-               ifelse( !is.null(logical4()) , logical4() , 
-                     ifelse( !is.null(logical3()) , logical3() , 
-                            ifelse( !is.null(logical2()) , logical2() , 'no matches found')))) })
+       ifelse( !is.na(logical5()) , logical5() , 
+               ifelse( !is.na(logical4()) , logical4() , 
+                     ifelse( !is.na(logical3()) , logical3() , 
+                            ifelse( !is.na(logical2()) , logical2() , 'no matches found')))) })
                 
 
     
@@ -98,38 +97,38 @@ shinyServer(function(input, output) {
     
     
     output$tabmesh <- renderDataTable({  
-      if( !is.null(tab5()) ) {tab5()}
-          else { if(!is.null(tab4())) {tab4()}
-               else { if(!is.null(tab3())) {tab3()}
-                    else { if(!is.null(tab2())) {tab2()} }}}
+      if( !is.na(tab5()) ) {tab5()}
+          else { if(!is.na(tab4())) {tab4()}
+               else { if(!is.na(tab3())) {tab3()}
+                    else { if(!is.na(tab2())) {tab2()} }}}
     } , options = list(orderClasses = TRUE , searching = FALSE , paging = FALSE))  
     
     
     tab2 <- reactive({
       if ( length(last_1()) == 1 ){
-        arrange(oracle2gram[ which( oracle2gram$leadingGram == tolower( trimws( paste(last_1() , collapse = ' ')))) , ] , -count)[1:input$sliderGuesses,]        
-      } 
+        arrange(oracle2gram[ which( oracle2gram$leading_Gram == tolower( trimws( paste(last_1() , collapse = ' ')))) , ] , -frequency)[1:input$sliderGuesses,]        
+      }  else {NA}
     })
   
   
     tab3 <- reactive({
       if ( length(last_2()) == 2 ){
-        arrange(oracle3gram[ which( oracle3gram$leadingGram == tolower( trimws( paste(last_2() , collapse = ' ')))) , ] , -count)[1:input$sliderGuesses,]        
-      } 
+        arrange(oracle3gram[ which( oracle3gram$leading_Gram == tolower( trimws( paste(last_2() , collapse = ' ')))) , ] , -frequency)[1:input$sliderGuesses,]        
+      }  else {NA}
     }) 
 
     
     tab4 <- reactive({
       if ( length(last_3()) == 3 ){
-        arrange(oracle4gram[ which( oracle4gram$leadingGram == tolower( trimws( paste(last_3() , collapse = ' ')))) , ] , -count)[1:input$sliderGuesses,]        
-      } 
+        arrange(oracle4gram[ which( oracle4gram$leading_Gram == tolower( trimws( paste(last_3() , collapse = ' ')))) , ] , -frequency)[1:input$sliderGuesses,]        
+      }  else {NA}
     }) 
    
  
     tab5 <- reactive({
       if ( length(last_4()) == 4  ){
-        arrange(oracle5gram[ which( oracle5gram$leadingGram == tolower( trimws( paste(last_4() , collapse = ' ')))) , ] , -count)[1:input$sliderGuesses,]        
-      } 
+        arrange(oracle5gram[ which( oracle5gram$leading_Gram == tolower( trimws( paste(last_4() , collapse = ' ')))) , ] , -frequency)[1:input$sliderGuesses,]        
+      }  else {NA}
     }) 
     
 ##### test table
